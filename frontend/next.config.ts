0d053f097fcb9973fb2cbef3bcbfc0a045665639
CS-Kiran <kiran2002bp@gmail.com>
2026-02-22 10:49:00 +0530

    Updating configuration files --frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactCompiler: true,
+  images: {
+    domains: ["localhost"],
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
 };
 
 export default nextConfig;
